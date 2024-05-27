8f222b730cfaf57973d345044a13aceec7c17d3c
Adwaith Ajith <141114182+Adwaith464@users.noreply.github.com>
2024-05-27 14:46:51 -0600

    Assignment2

---
+++ app/week-2/student-info.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+    return (
+        <div>
+            <p>ADWAITH SHINI AJITH</p>
+            <Link href="https://github.com/Adwaith0406?tab=repositories">MY GITHUB REPOSITORIES</Link>
+        </div>
+    );
+}
