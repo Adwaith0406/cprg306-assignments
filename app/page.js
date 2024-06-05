8a19f80bba0960ada44650c966be48a39b29d7d8
Adwaith Ajith <141114182+Adwaith464@users.noreply.github.com>
2024-06-05 15:47:16 -0600

    Week 3

--- app/page.js
+++ app/page.js
@@ -3,6 +3,8 @@ import Link from "next/link"
 export default function Root(){
   return(
     <><h1>CPRG 306: Web Development 2 - Assignments</h1>
-    <Link href="/week-2">Week 2 Assignment</Link></>
+    <Link href="/week-2">Week 2 Assignment</Link>
+    <Link href="/week-3">Week 3 Assignment</Link>
+    </>
   )
 }
