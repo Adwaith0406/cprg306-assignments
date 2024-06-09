13279762fd4f85accc620670713a77f3f24009a7
Adwaith Ajith <141114182+Adwaith464@users.noreply.github.com>
2024-06-09 15:41:35 -0600

    Week 4

--- app/page.js
+++ app/page.js
@@ -2,9 +2,12 @@ import Link from "next/link"
 
 export default function Root(){
   return(
-    <><h1>CPRG 306: Web Development 2 - Assignments</h1>
+    <div><h1>CPRG 306: Web Development 2 - Assignments</h1>
     <Link href="/week-2">Week 2 Assignment</Link>
+    <br></br>
     <Link href="/week-3">Week 3 Assignment</Link>
-    </>
+    <br></br>
+    <Link href="/week-4">Week 4 Assignment</Link>
+    </div>
   )
 }
---
+++ app/week-4/page.js
@@ -0,0 +1,10 @@
+import NewItem from './new-item';
+
+export default function Week4() {
+    return (
+        <div>
+            <br></br>
+            <NewItem />
+        </div>
+    );
+}
