a5643d13ad33a718b31f3521167e439e2c9fc35e
Adwaith Ajith <141114182+Adwaith464@users.noreply.github.com>
2024-06-17 04:00:39 -0600

    Week 5 Assignment

--- app/page.js
+++ app/page.js
@@ -8,6 +8,8 @@ export default function Root(){
     <Link href="/week-3">Week 3 Assignment</Link>
     <br></br>
     <Link href="/week-4">Week 4 Assignment</Link>
+    <br></br>
+    <Link href={"/week-5"}>Week 5 Assignment</Link>
     </div>
   )
 }
---
+++ app/week-5/item.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Item = ({ name, quantity, category }) => {
+  return (
+    <li className="bg-gray-800 text-white p-4 m-2 lg w-80">
+      <div className="flex justify-between">
+        <span>{name}</span>
+      </div>
+      <div className="text-sm text-gray-400">
+        Buy {quantity} in {category}
+      </div>
+    </li>
+  );
+};
+
+export default Item;
